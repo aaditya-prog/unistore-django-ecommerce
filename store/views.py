53ef9dbb8a085e364ec673028a9d49c14387d41c
Aaditya Dulal <np01cp4a190231@islingtoncollege.edu.np>
2021-07-28 07:30:17 +0545

    Pagination

--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
+from itertools import product
 from django.shortcuts import render, redirect
 from .models import Product, Category
 from django.views import View
-from .filters import ProductFilter 
+from .filters import ProductFilter
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     products = None
@@ -14,12 +16,18 @@ def index(request):
     elif sort:
         products = Product.objects.filter().order_by(sort)      
     else:
-        products = Product.get_all_products()
+        products = Product.get_all_products().order_by("id")
+    paginator = Paginator(products,6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     myFilter = ProductFilter(request.GET, queryset=products)
     products = myFilter.qs
+    
     data={}
     data["products"] = products
     data["categories"] = categories
     data["myFilter"] = myFilter
+    data["page_obj"] = page_obj
     return render(request, "store/index.html",data)
 
